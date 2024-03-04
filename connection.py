a883400c6a39b9a3020ebbefa7e862121c2077f8
aldouv4 <156018138+aldouv4@users.noreply.github.com>
2024-03-04 10:21:49 -0700

    Update connection.py
    Description and comments to the code

--- connection.py
+++ connection.py
@@ -3,14 +3,21 @@ import mysql.connector as mysqlc
 
 
 class Connection_mysql :
+''' This class defines a way to connect to MySQL database'''
         
-    def __init__ ( self , user = 'root' , password = 'lapislazuli4' , host = 'localhost' , database = 'empresa' ) :
+    def __init__ ( self , user , password  , host , database ) :    
+    '''Give the necessary parameters to acces to database including :
+    user name, password, url or ip for the database host and the database name'''
 
+        #Create string for the engine
+        
         db_connection_str ='mysql+pymysql://'+ user+ ':' + password + '@' + host + '/' + database 
+
+        # Set the variable db_connection as an attribute 
 
         self.db_connection = create_engine(db_connection_str)
 
-        # def dispose ( self ) : 
+        
 
 
 
