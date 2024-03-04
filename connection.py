619655e0e83fd6c89dc06b7f5b407b3deb2fbd4d
aldouv4 <156018138+aldouv4@users.noreply.github.com>
2024-03-04 13:43:52 -0700

    Format corrections

--- connection.py
+++ connection.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 
 class Connection_mysql :
-''' This class defines a way to connect to MySQL database'''
+    ''' This class defines a way to connect to MySQL database'''
 #------------------------------------------------------------------------------------------------------------------------------------------------       
    
-    def __init__ ( self , user , password  , host , database ) :    
-    '''Give the necessary parameters to acces to database including :
-    user name, password, url or ip for the database host and the database name'''
+    def __init__ ( self , user , password  , host , database ) :
+        '''Give the necessary parameters to acces to database including :
+        user name, password, url or ip for the database host and the database name'''
 
         #Create string for the engine
         
@@ -21,11 +21,9 @@ class Connection_mysql :
 #------------------------------------------------------------------------------------------------------------------------------------------------
 
     def read_data ( self , query_extract_data ) :
-    
-    '''Creates a pandas DataFrame from MySQL database table. Example
-    table_name = table_name
-    query_extract_data = f'SELECT * FROM {table_name}'
-    '''
+        '''Creates a pandas DataFrame from MySQL database table. Example
+        table_name = table_name
+        query_extract_data = f'SELECT * FROM {table_name}' '''
 
         df = pd.sql_read ( query_extract_data , self.db_connection )
 
@@ -33,12 +31,10 @@ class Connection_mysql :
 #------------------------------------------------------------------------------------------------------------------------------------------------
 
     def delete_data (self , selected_table , conditional_column , condition ) :
-
         ''' Delete rows depending on selected condition
         selected_table = str, database table to delet rows
         conditional_column = str , column containing the conditional statement 
-        condition = str, conditional value or values selected to drop from the table
-        '''
+        condition = str, conditional value or values selected to drop from the table '''
         #Set the query statement for rows deletion
         query_delete = '''DELETE FROM ''' + selected_table + ''' WHERE ''' + conditional_column + ''' IN ''' + f'(condition)'
 
@@ -48,11 +44,9 @@ class Connection_mysql :
 #------------------------------------------------------------------------------------------------------------------------------------------------
            
     def upload_data ( self , df , selected_table ) :
-    
         '''Upload multiple rows from a pandas DataFrame to selected database table from MySQL
         df= pd DataFrame, contains the rows to upload
-        selected_table = str, MySQL table name to upload values        
-        '''
+        selected_table = str, MySQL table name to upload values '''
 
         df.to_sql ( 
             con = self.db_connection,  #Define the connection
@@ -75,14 +69,13 @@ class Connection_mysql :
         
     def delete_Table ( self, table_name ): 
         '''Deletes table from database
-        table_name = str, table to be dropped
-        '''
+        table_name = str, table to be dropped'''
+        
         #Execute query to drop the selected table, add statement 'if exists' 
         db_connection.execute ( '''DROP TABLE IF EXISTS ''' + table_name  )
  #------------------------------------------------------------------------------------------------------------------------------------------------
  
-    def dispose ( self ) : 
-    
-    ''' Dispose connection''' 
+    def dispose ( self ) :     
+        ''' Dispose connection''' 
    
         self.db_connection.dispose ( )
