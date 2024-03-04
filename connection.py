018d380499442f32ac09a8e7e6540fe22c552db3
aldouv4 <156018138+aldouv4@users.noreply.github.com>
2024-03-04 13:41:13 -0700

    Add CRUDE methods to the class

--- connection.py
+++ connection.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 class Connection_mysql :
 ''' This class defines a way to connect to MySQL database'''
-        
+#------------------------------------------------------------------------------------------------------------------------------------------------       
+   
     def __init__ ( self , user , password  , host , database ) :    
     '''Give the necessary parameters to acces to database including :
     user name, password, url or ip for the database host and the database name'''
@@ -17,18 +18,71 @@ class Connection_mysql :
         # Set the variable db_connection as an attribute 
 
         self.db_connection = create_engine(db_connection_str)
+#------------------------------------------------------------------------------------------------------------------------------------------------
 
     def read_data ( self , query_extract_data ) :
+    
     '''Creates a pandas DataFrame from MySQL database table. Example
-        table_name = table_name
-        query_extract_data = f'SELECT * FROM {table_name}' '''
+    table_name = table_name
+    query_extract_data = f'SELECT * FROM {table_name}'
+    '''
 
         df = pd.sql_read ( query_extract_data , self.db_connection )
 
         return df
+#------------------------------------------------------------------------------------------------------------------------------------------------
 
+    def delete_data (self , selected_table , conditional_column , condition ) :
 
+        ''' Delete rows depending on selected condition
+        selected_table = str, database table to delet rows
+        conditional_column = str , column containing the conditional statement 
+        condition = str, conditional value or values selected to drop from the table
+        '''
+        #Set the query statement for rows deletion
+        query_delete = '''DELETE FROM ''' + selected_table + ''' WHERE ''' + conditional_column + ''' IN ''' + f'(condition)'
+
+        #Execute query
+
+        self.db_connection.execute ( query_delete )
+#------------------------------------------------------------------------------------------------------------------------------------------------
+           
+    def upload_data ( self , df , selected_table ) :
+    
+        '''Upload multiple rows from a pandas DataFrame to selected database table from MySQL
+        df= pd DataFrame, contains the rows to upload
+        selected_table = str, MySQL table name to upload values        
+        '''
+
+        df.to_sql ( 
+            con = self.db_connection,  #Define the connection
+            name = selected_table ,    #Defines selected table
+            if_exists = 'append',      #Declares append if it exists, otherwise the table is created
+            index = False,             #Avoids upload df index to the table
+            chunksize=500,             #Batch uploading, if number of rows is too big it avoids pc burnout
+            method ='multi'            #Defines multiple rows are appended
+        )    
+#------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    def create_table (self , table_name , columns_names ):
+        '''Creates a new table for the database
+        table_name : str, name of the new table
+        columns_names = str, names and type of each column from the table'''
+
+        #Create table with the statement 'if not exists'    
+        db_connection.execute ( '''CREATE TABLE if not exists ''' + table_name +  f'(columns_names)' )
+#------------------------------------------------------------------------------------------------------------------------------------------------
         
-
-
-
+    def delete_Table ( self, table_name ): 
+        '''Deletes table from database
+        table_name = str, table to be dropped
+        '''
+        #Execute query to drop the selected table, add statement 'if exists' 
+        db_connection.execute ( '''DROP TABLE IF EXISTS ''' + table_name  )
+ #------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+    def dispose ( self ) : 
+    
+    ''' Dispose connection''' 
+   
+        self.db_connection.dispose ( )
